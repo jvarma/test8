6c23c738826305a464ab0efba2b5ed29ab4b66c4
Jayant Varma <jayant.1971@gmail.com>
2014-11-30 22:46:27 +0530

    added single hotel display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,11 @@ class PagesController < ApplicationController
 		@page = "home"
 		gon.lat = 28.57
 		gon.lng = 77.32
+		hostname = request.host_with_port()
+		gas_station_url = "http://"+hostname+"/gas-stations"
+		movies_url = "http://"+hostname+"/movies"
+		hotels_url = "http://"+hostname+"/hotels"
+		coupons_url = "http://"+hostname+"/coupons"
 		gon.heading = "Within 5 miles of your location"
 		gon.infoWindowContent = '<div class="infowindow">'+
       	'<div id="bodyContent">'+
@@ -14,13 +19,13 @@ class PagesController < ApplicationController
   			'<table class="table">'+
 				'<tr>'+
 					'<td align="center">'+
-						'<a href="http://www.latrattoriawv.com/10-off-coupon/"><img src="http://localhost:3000/coupons/coupon_1.jpg"/></a>'+
+						'<a href="http://www.latrattoriawv.com/10-off-coupon/"><img src="'+coupons_url+'/coupon_1.jpg"/></a>'+
 					'</td>'+
 					'<td align="center">'+
-						'<a href="http://www.partycity.com/category/entertaining+serving.do"><img src="http://localhost:3000/coupons/coupon_2.jpg"/></a>'+
+						'<a href="http://www.partycity.com/category/entertaining+serving.do"><img src="'+coupons_url+'/coupon_2.jpg"/></a>'+
 					'</td>'+
 					'<td align="center">'+
-						'<a href="http://www.partycity.com/category/entertaining+serving.do"><img src="http://localhost:3000/coupons/coupon_3.jpg"/></a>'+
+						'<a href="http://www.partycity.com/category/entertaining+serving.do"><img src="'+coupons_url+'/coupon_3.jpg"/></a>'+
 					'</td>'+
 				'</tr>'+
 			'</table>'+
@@ -35,10 +40,10 @@ class PagesController < ApplicationController
 		'<div class="panel panel-info">'+
   			'<div class="panel-heading"><b>Around you...</b></div>'+
   			'<ul class="list-group">'+
-    			'<li class="list-group-item"><a href="http://localhost:3000/gas-stations"><big>3 gas stations</big></a></li>'+
+    			'<li class="list-group-item"><a href="'+gas_station_url+'"><big>3 gas stations</big></a></li>'+
     			'<li class="list-group-item"><a href=""><big>5 restaurants</big></a></li>'+
-    			'<li class="list-group-item"><a href="http://localhost:3000/movies"><big>1 movie theatre</big></a></li>'+
-    			'<li class="list-group-item"><a href=""><big>5 bars</big></a></li>'+
+    			'<li class="list-group-item"><a href="'+movies_url+'"><big>1 movie theatre</big></a></li>'+
+    			'<li class="list-group-item"><a href="'+hotels_url+'"><big>3 hotels</big></a></li>'+
   			'</ul>'+
 		'</div>'+
       '</div>'+
@@ -49,6 +54,8 @@ class PagesController < ApplicationController
 
 	def gas_stations
 		@page = "gas"
+		gon.hostname = request.host_with_port()
+
 
 		gon.lat = 28.57
 		gon.lng = 77.32
@@ -73,6 +80,7 @@ class PagesController < ApplicationController
 
 
 	def movies
+		gon.hostname = request.host_with_port()
 		@page = "movie"
 		gon.lat = 28.57
 		gon.lng = 77.32
@@ -81,4 +89,18 @@ class PagesController < ApplicationController
 		gon.theatre_lng = 77.3181
 		
 	end
+
+	def hotels
+		@page = "hotel"
+		gon.hostname = request.host_with_port()
+
+
+		gon.lat = 28.57
+		gon.lng = 77.32
+		
+		gon.hotel_lat = 28.5701
+		gon.hotel_lng = 77.3181
+		
+	end
+
 end
